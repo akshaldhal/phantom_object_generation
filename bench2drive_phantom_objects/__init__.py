a3bd30da7cfc7e4b6068960127bf56fd64c759a9
akshaldhal <86314014+akshaldhal@users.noreply.github.com>
2026-03-06 02:14:03 -0500

    Version Update

--- bench2drive_phantom_objects/__init__.py
+++ bench2drive_phantom_objects/__init__.py
@@ -1,16 +1,19 @@
-__version__ = "0.2.0"
-
+__version__ = "0.2.1"
 from .dataset_builder import DatasetBuilder, LidarConfig, build_mask
 from .download_dataset import DatasetSize, download_bech2drive_dataset
-# from .laz_utils import laz_to_obj
+# from .laz_util import check as check_laz
+# from .laz_util import compare as compare_laz
+# from .laz_util import laz_to_obj
 from .lidar_recorder import LidarRecorder
 
 __all__ = [
     "download_bech2drive_dataset",
     "DatasetSize",
     "LidarRecorder",
-    # "laz_to_obj",
     "LidarConfig",
     "DatasetBuilder",
     "build_mask",
+    # "laz_to_obj",
+    # "check_laz",
+    # "compare_laz",
 ]
